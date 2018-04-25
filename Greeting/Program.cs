5c992c1cc2dce9ad01baa8631253574694b82fd5
ProgrammingCamp <38556895+ProgrammingCamp@users.noreply.github.com>
2018-04-25 09:32:03 +0300

    Initial Commit

---
+++ Greeting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string fullname = Console.ReadLine();
+
+            string greeting = "Welcome " + fullname;
+            Console.WriteLine(greeting);
+
+            Console.ReadLine();
+        }
+    }
+}
